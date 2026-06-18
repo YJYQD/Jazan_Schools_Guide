329b227539e7b1e452eceb55773fc5edfe4b37bb
YAHYA MOHAMMED YAHYA MAKRASHI <wwf600600@gmail.com>
2026-06-18 14:49:48 +0300

    تم
    تعديل

--- if0_41747985_jazan_db (1).sql
+++ if0_41747985_jazan_db (1).sql
@@ -1,31 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: 20 مايو 2026 الساعة 21:33
--- إصدار الخادم: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `if0_41747985_jazan_db`
---
-
--- --------------------------------------------------------
-
---
--- بنية الجدول `audit_trail`
---
 
 CREATE TABLE `audit_trail` (
   `Audit_ID` int(11) NOT NULL,
